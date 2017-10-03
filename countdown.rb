2085d49c65ce47c8e47ab83f0ed3e1c185326db3
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-03 15:10:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(second_count)
+  while second_count > 0
+    puts "#{second_count} SECOND(S)!"
+    second_count -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(second_count)
+  while second_count > 0
+    puts "#{second_count} SECOND(S)!"
+    sleep 1
+    second_count -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
